038064bedba41e9deb051c69941aa1f1f06ebb2e
Daniel Lomis <daniellomis@yahoo.com>
2022-10-16 02:23:33 -0400

    Further Syntax Updates and Code Updates

--- Assembly/ASM80/Binary Adder.8080.asm
+++ Assembly/ASM80/Binary Adder.8080.asm
@@ -5,30 +5,34 @@
 ;   Location: McLean, Virginia
 ;   Company: The Lomis Company of Virginia, R&D
 ;
-;   Date Successfully Compiled on the Altair: 10/15/20222
+;   Last Date Successfully Compiled: 10/15/20222
 ;
 ;   Programming Language: Assembly
 ;   Intruction Code Language: ASM80
 ;   Programming Syntax: CP/M Assembler Mnemonics (Intel-based)
-;   System Architecture: Intel 8080
+;   System Architecture: Intel 8080 Microprocessor
 ;   Compiler: CP/M ASSEMBLER - VER 2.0
 ;
 ;   Target Comuter: Altair 8800
 ;
-;   Description: A front panel program for adding two 8-Bit words, A and B, yielding 
+;   Description: A front panel program for adding two words (8-Bits each), A and B, yielding 
 ;                an output, C. My first program written in Assembly. Runs on any Intel 8080-based computer.
+;                System can be reset with lifting STOP and RESET together, this will cause the Program Counter
+;                to reset  back to Memory Address 0c000000, revealing the answer to the operator.
 ;
 ;       Mathematical Formula: A + B = C
 ;
 ;       Input Memory Address  A, B: 0x0100, 0x0102
 ;       Output Memory Address C: 0x0104
 ;
-;VARIABLE       |MNEMONIC   |OPERAND         |COMMENT(S)
-                LHLD        000001Q          ; Writes the value located at memory address 0x0100 to register L
-                LDA         000002Q          ; Writes the value located at memory address 0x0101 to register A
-                ORG         000007Q          ; Sets the CPU's program counter to 0. Programming will be entered from memory address 0x0000 onward.
-                ADD         L                ; Performs binary or decimal addition of register A and L. Writes the output to register A.
-                STA         000000Q          ; Copies the contents of register A and writes it to memory address 0x0104  (0104)
-                ORG         000017Q          ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0x0040 onward.
-                HLT                          ; Halt the CPU
-                END                          ; End of Assembly Intruction
+;VARIABLE|      |MNEMONIC   |OPERAND        |COMMENT(S)
+;   OUT-C:      EQU         000000Q         | OUTPUT C - Result of Register A is outputed here (answer)
+;    IN-A:      EQU         000001Q         | INPUT A - First word (binary/decimal)
+;    IN-B:      EQU         000002Q         | INPUT B - Second word (binary/decimal)
+                ORG         000007Q         ; Sets the 8080's Program Counter to 0o000007. Programming will be entered from memory address 0o000007 onward
+                LHLD        000001Q         ; Writes the value located at memory address 0o000001 to Register L (IN-A)
+                LDA         000002Q         ; Writes the value located at memory address 0o000002 to Register A (IN-B)
+                ADD         L               ; Performs binary/decimal addition of Register A and L, replacing its answer back into Register A
+                STA         000000Q         ; Copies Register A's contents and writes it to memory address 0o000000 (OUT-C)
+                HLT                         ; Halt the 8080
+                END                         ; End of Assembly Intruction
--- Assembly/ASM80/Memory Wiper.8080.asm
+++ Assembly/ASM80/Memory Wiper.8080.asm
@@ -5,32 +5,31 @@
 ;   Location: McLean, Virginia
 ;   Company: The Lomis Company of Virginia, R&D
 ;
-;   Date Successfully Compiled on the Altair: 10/15/2022
+;   Last Date Successfully Compiled: 10/15/2022
 ;
 ;   Programming Language: Assembly
-;   Intruction Code Language: ASM80
+;   Instruction Code Language: ASM80
 ;   Programming Syntax: CP/M Assembler Mnemonics (Intel-based)
-;   System Architecture: Intel 8080
+;   System Architecture: Intel 8080 Microprocessor
 ;   Compiler: CP/M ASSEMBLER - VER 2.0
 ;
 ;   Target Computer: Altair 8800
 ;
 ;   Description:  A memory eraser program. It essentially clears the entire system of
 ;                 memory. It will perform either one or all of the erasures, as defined,
-;                 upto 16 times. Supports writing zeros, ones, alternating zeros and ones,
+;                 up to 16 times. Supports writing zeros, ones, alternating zeros and ones,
 ;                 alternating ones and zeros, and custom value.
 ;
-;VARIABLE       |MNEMONIC   |OPERAND         |COMMENT(S)
-                ORG         00FFH            ; Sets the CPU's program counter to 00FF. Programming will be entered from memory address 00FF onward
-REGVARI:        EQU         01FFH            ; Global variable, indicates which register to write the memory pattern with at memory address 01FF
-                MVI         B,0011H          ; Writes the value 0055 [01010101, base-2] to register B
-                MVI         C,00AAH          ; Writes the value 00AA [10101010, base-2] to register C
-                MVI         D,0000H          ; Writes the value 0000 [00000000, base-2] to register D
-                MVI         E,00FFH          ; Writes the value 00FF [11111111, base-2] to register E
-                LDA         0F00H            ; Writes the value contained at at memory address 0F00 to register A, designed for custom bit writing
-                LXI         H,0000H          ; Writes the value 0000 [0000000000000000, base-2] to register pair HL
-                INR         M                ; Increment the H-L register by 1, forcing the pair to point at the adjacent memory address
-                JMP         0010H            ; Jumps the program counter to memory address 0010.
-                ORG         0040H            ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0040 onward.
-                HLT                          ; Halt the CPU
-                END                          ; End of Assembly Intruction
+;VARIABLE|      |MNEMONIC   |OPERAND        |COMMENT(S)
+  REGVARI:      EQU         01FFH           ; Global variable, indicates which register to write the memory pattern with at memory address 01FF
+                MVI         B,0011H         ; Writes the value 0055 [01010101, base-2] to register B
+                MVI         C,00AAH         ; Writes the value 00AA [10101010, base-2] to register C
+                MVI         D,0000H         ; Writes the value 0000 [00000000, base-2] to register D
+                MVI         E,00FFH         ; Writes the value 00FF [11111111, base-2] to register E
+                LDA         0F00H           ; Writes the value contained at at memory address 0F00 to register A, designed for custom bit writing
+                LXI         H,0000H         ; Writes the value 0000 [0000000000000000, base-2] to register pair HL
+                INR         M               ; Increment the H-L register by 1, forcing the pair to point at the adjacent memory address
+                JMP         0010H           ; Jumps the program counter to memory address 0010.
+                ORG         0040H           ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0040 onward.
+                HLT                         ; Halt the CPU
+                END                         ; End of Assembly Instruction
