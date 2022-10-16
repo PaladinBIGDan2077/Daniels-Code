58c99ebe4972fd2a3880d80f9efa489b27cbd7d1
Daniel Lomis <daniellomis@yahoo.com>
2022-10-16 00:46:24 -0400

    Further code updating, syntax fixes

--- Assembly/ASM80/Binary Adder.8080.asm
+++ Assembly/ASM80/Binary Adder.8080.asm
@@ -5,11 +5,11 @@
 ;   Location: McLean, Virginia
 ;   Company: The Lomis Company of Virginia, R&D
 ;
-;   Date Successfully Compiled on the Altair:
+;   Date Successfully Compiled on the Altair: 10/15/20222
 ;
 ;   Programming Language: Assembly
 ;   Intruction Code Language: ASM80
-;   Programming Syntax: INTEL MNEUMONICS
+;   Programming Syntax: CP/M Assembler Mnemonics (Intel-based)
 ;   System Architecture: Intel 8080
 ;   Compiler: CP/M ASSEMBLER - VER 2.0
 ;
@@ -23,13 +23,12 @@
 ;       Input Memory Address  A, B: 0x0100, 0x0102
 ;       Output Memory Address C: 0x0104
 ;
-;|LABEL     |MNEMONIC   |OPERAND    |COMMENTS
-            ORG         00FFH       ;  Sets the CPU's program counter to 0. Programming will be entered from memory address 0x0000 onward.
-            LHLD        0100H	    ;  Writes the value located at memory address 0x0100 to register L
-	        LDA         0102H       ;  Writes the value located at memory address 0x0101 to register A
-            ADD         L           ;  Performs binary or decimal addition of register A and L. Writes the output to register A.
-            STA         0104H       ;  Copies the contents of register A and writes it to memory address 0x0104  (0104)
-            JMP         0010H       ;  Jumps the program counter to memory address 0x0010.
-            ORG         0040H       ;  Sets the CPU's program counter to 40. Programming will be entered from memory address 0x0040 onward.
-            HLT                     ;  halt the CPU
-            END                     ;  End of Assembly Intruction
+;VARIABLE       |MNEMONIC   |OPERAND         |COMMENT(S)
+                LHLD        000001Q          ; Writes the value located at memory address 0x0100 to register L
+                LDA         000002Q          ; Writes the value located at memory address 0x0101 to register A
+                ORG         000007Q          ; Sets the CPU's program counter to 0. Programming will be entered from memory address 0x0000 onward.
+                ADD         L                ; Performs binary or decimal addition of register A and L. Writes the output to register A.
+                STA         000000Q          ; Copies the contents of register A and writes it to memory address 0x0104  (0104)
+                ORG         000017Q          ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0x0040 onward.
+                HLT                          ; Halt the CPU
+                END                          ; End of Assembly Intruction
--- Assembly/ASM80/Memory Wiper.8080.asm
+++ Assembly/ASM80/Memory Wiper.8080.asm
@@ -9,7 +9,7 @@
 ;
 ;   Programming Language: Assembly
 ;   Intruction Code Language: ASM80
-;   Programming Syntax: INTEL MNEUMONICS
+;   Programming Syntax: CP/M Assembler Mnemonics (Intel-based)
 ;   System Architecture: Intel 8080
 ;   Compiler: CP/M ASSEMBLER - VER 2.0
 ;
@@ -20,17 +20,17 @@
 ;                 upto 16 times. Supports writing zeros, ones, alternating zeros and ones,
 ;                 alternating ones and zeros, and custom value.
 ;
-;|LABEL     |MNEMONIC   |OPERAND    |COMMENTS
-            ORG         00FFH       ; Sets the CPU's program counter to 00FF. Programming will be entered from memory address 00FF onward
-REGVARI:    EQU         01FFH       ; Global variable, indicates which register to write the memory pattern with at memory address 01FF
-            MVI         B,0011H     ; Writes the value 0055 [01010101, base-2] to register B
-            MVI         C,00AAH     ; Writes the value 00AA [10101010, base-2] to register C
-            MVI         D,0000H     ; Writes the value 0000 [00000000, base-2] to register D
-            MVI         E,00FFH     ; Writes the value 00FF [11111111, base-2] to register E
-            LDA         0F00H       ; Writes the value contained at at memory address 0F00 to register A, designed for custom bit writing
-            LXI         H,0000H     ; Writes the value 0000 [0000000000000000, base-2] to register pair HL
-            INR         M           ; Increment the H-L register by 1, forcing the pair to point at the adjacent memory address
-            JMP         0010H       ; Jumps the program counter to memory address 0010.
-            ORG         0040H       ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0040 onward.
-            HLT                     ; Halt the CPU
-            END                     ; End of Assembly Intruction
+;VARIABLE       |MNEMONIC   |OPERAND         |COMMENT(S)
+                ORG         00FFH            ; Sets the CPU's program counter to 00FF. Programming will be entered from memory address 00FF onward
+REGVARI:        EQU         01FFH            ; Global variable, indicates which register to write the memory pattern with at memory address 01FF
+                MVI         B,0011H          ; Writes the value 0055 [01010101, base-2] to register B
+                MVI         C,00AAH          ; Writes the value 00AA [10101010, base-2] to register C
+                MVI         D,0000H          ; Writes the value 0000 [00000000, base-2] to register D
+                MVI         E,00FFH          ; Writes the value 00FF [11111111, base-2] to register E
+                LDA         0F00H            ; Writes the value contained at at memory address 0F00 to register A, designed for custom bit writing
+                LXI         H,0000H          ; Writes the value 0000 [0000000000000000, base-2] to register pair HL
+                INR         M                ; Increment the H-L register by 1, forcing the pair to point at the adjacent memory address
+                JMP         0010H            ; Jumps the program counter to memory address 0010.
+                ORG         0040H            ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0040 onward.
+                HLT                          ; Halt the CPU
+                END                          ; End of Assembly Intruction
