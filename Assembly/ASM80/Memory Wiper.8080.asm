462bc721741af97347734703bf85f759837f38a9
Daniel Lomis <daniellomis@yahoo.com>
2022-10-24 14:01:56 -0400

    Changes

--- Assembly/ASM80/Memory Wiper.8080.asm
+++ Assembly/ASM80/Memory Wiper.8080.asm
@@ -1,4 +1,4 @@
-;                                                                                                       <
+;                                                                                                       
 ;   Title: Memory Wiper (WIP)
 ;   Developer: Daniel J. Lomis
 ;   Date Written: October 13, 2022
@@ -20,16 +20,24 @@
 ;                 up to 16 times. Supports writing zeros, ones, alternating zeros and ones,
 ;                 alternating ones and zeros, and custom value.
 ;
-;VARIABLE|      |MNEMONIC   |OPERAND        |COMMENT(S)
-   REGVAR:      EQU         01FFH           ; Global variable, indicates which register to write the memory pattern with at memory address 01FF
-                MVI         B,0011H         ; Writes the value 0055 [01010101, base-2] to register B
-                MVI         C,00AAH         ; Writes the value 00AA [10101010, base-2] to register C
-                MVI         D,0000H         ; Writes the value 0000 [00000000, base-2] to register D
-                MVI         E,00FFH         ; Writes the value 00FF [11111111, base-2] to register E
-                LDA         0F00H           ; Writes the value contained at at memory address 0F00 to register A, designed for custom bit writing
-                LXI         H,0000H         ; Writes the value 0000 [0000000000000000, base-2] to register pair HL
-                INR         M               ; Increment the H-L register by 1, forcing the pair to point at the adjacent memory address
-                JMP         0010H           ; Jumps the program counter to memory address 0010.
-                ORG         0040H           ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0040 onward.
-                HLT                         ; Halt the CPU
-                END                         ; End of Assembly Instruction
+;  VARIABLE|      |MNEMONIC   |OPERAND        |COMMENT(S)
+     REGVAR:      EQU         0000H           ; Global Var, indicates where to write Register val.
+                  ORG         0001H           ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0040 onward.
+                  MVI         B,0011H         ; Writes value 0055 [01010101, base-2] to Register B
+                  MVI         C,00AAH         ; Writes value 00AA [10101010, base-2] to Register C
+                  MVI         D,0000H         ; Writes value 0000 [00000000, base-2] to Register D
+                  MVI         E,00FFH         ; Writes value 00FF [11111111, base-2] to Register E
+                  LXI         H,0000H         ; Writes value 0000 to register pair HL
+                  JMP         SUBROUTINE      ; Jumps Program Counter to Subroutine function.
+                  IN          
+   CHECKREG:      IF          REGVAR
+ 
+ 
+       LOOP:
+                  MOV         
+                  LDA         0001H           ; Writes value at Address 0x0001 to Register A
+                  INR         M               ; Increment the H-L register by 1, forcing the pair to point at the adjacent memory address
+                  JMP         0010H           ; Jumps the program counter to memory address 0010.
+                  ORG         0040H           ; Sets the CPU's program counter to 40. Programming will be entered from memory address 0040 onward.
+                  HLT                         ; Halt the CPU
+                  END                         ; End of Assembly Instruction
