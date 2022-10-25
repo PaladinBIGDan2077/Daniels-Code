14cabc5a2ec438629fa3f7f37306e4edaf06f86f
Daniel Lomis <daniellomis@yahoo.com>
2022-10-25 11:18:50 -0400

    Folder Restructuring

---
+++ Python/Scanline Demo.py
@@ -0,0 +1,24 @@
+import os
+from time import sleep
+
+
+def cheese():
+    sleep(0.25)
+
+for i in range(8):
+    print("___#___")
+    cheese()
+    print("__#_#__")
+    cheese()
+    print("_#___#_")
+    cheese()
+    print("#_____#")
+    cheese()
+    print("#######")
+    cheese()
+    print("#_____#")
+    cheese()
+    print("#_____#")
+    cheese()
+    print("_______")
+    os.system("clear")
