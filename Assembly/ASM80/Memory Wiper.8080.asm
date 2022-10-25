6a63eabead204414c5ac62dda98a0fc69c3c1549
Daniel Lomis <daniellomis@yahoo.com>
2022-10-25 00:53:01 -0400

    Fixing Bugs

--- Assembly/ASM80/Memory Wiper.8080.asm
+++ Assembly/ASM80/Memory Wiper.8080.asm
@@ -5,7 +5,7 @@
 ;   Location: McLean, Virginia
 ;   Company: The Lomis Company of Virginia, R&D
 ;
-;   Last Successful Compile: 10/16/20222
+;   Last Successful Compile: 12:12AM 10/25/20222
 ;
 ;   Programming Language: Assembly
 ;   Instruction Code Language: ASM80
@@ -20,6 +20,12 @@
 ;                 up to 16 times. Supports writing zeros, ones, alternating zeros and ones,
 ;                 alternating ones and zeros, and custom value.
 ;
+;                 Usable Register Codes (Set at address 0x0000)
+;                 B [000] => [01010101]
+;                 C [001] => [10101010]
+;                 D [010] => [00000000]
+;                 E [011] => [11111111]
+;
 ;  VARIABLE|    |MNEMONIC   |OPERAND        |COMMENT(S)
      REGVAR:    DS          0000H           ; Global Var, indicates where to write Register val.
                 ORG         0FFFFH          ;
@@ -30,7 +36,7 @@
                 MVI         D,0000H         ; Writes value 0000 [00000000, base-2] to Register D
                 MVI         E,00FFH         ; Writes value 00FF [11111111, base-2] to Register E
                 LXI         H,0020H         ; Writes value 0020 to register pair HL
-                MOV         A,REGVAR        ;
+                MOV         A,E             ; TEMP SOLN. DEBUG, E REPLACES 0000H
                 JMP         WRITER          ; Jumps Program Counter to WRITER function.
                 HLT                         ; Halt the CPU
 ;
@@ -38,5 +44,6 @@
                 MOV         M,A             ; Writes value at Address 0x0001 to Register A
                 INR         M               ; Increment the H-L register by 1, forcing the pair to point at the adjacent memory addres                          s
                 RC                          ; Jumps the program counter to memory address 0010.
+                JMP         WRITER
 ;
                 END                         ; End of Assembly Instruction
